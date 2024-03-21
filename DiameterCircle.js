90606a52a1403f997308f3f8e00912d6f77d4c97
saajan <2000gurbhejsingh2311@gmail.com>
2024-03-21 18:50:15 +0530

    DiameterCircle

---
+++ DiameterCircle.js
@@ -0,0 +1,10 @@
+                                    // Write a function to find the diameter of the circle. 
+                                    // Given radius of the circle is 10.
+
+function findDiameter(radius){
+    return radius * 2
+}
+
+let radius = 10;
+
+console.log('the diameter is',findDiameter(radius))
