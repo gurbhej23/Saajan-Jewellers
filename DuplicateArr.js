03dd742a00f3afc496f480143d5f8f0a7d0b2b31
saajan <2000gurbhejsingh2311@gmail.com>
2024-04-30 16:30:48 +0530

    NewPages

--- DuplicateArr.js
+++
@@ -1,8 +0,0 @@
-                            // Write a javascript program to find duplicate values in array. 
-                                    // Ex. let arr = [23,26,22,45,1,22,33]
-let arr = [23,26,22,45,1,22,33]
-let result = arr.filter((val,index,self)=>{
-  return self.indexOf(val) !== index;
-})
-
-console.log( result )
---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('wish-list').addEventListener('click',function(){
+    alert('this item is add in fav list')
+})  
